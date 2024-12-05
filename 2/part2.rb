269f5b91757ccca7aef99f8f7aafe7554f7477a4
Anthony Avina <anthony@aavina.com>
2024-12-05 00:07:29 -0600

    Cleaned up part2 day2

--- 2/part2.rb
+++ 2/part2.rb
@@ -29,32 +29,19 @@ def is_report_safe?(elements:)
 
     previous_element = elements[idx - 1].to_i
 
-    if previous_element == current
-      safe = false
-      break
-    end
+    safe = false && break if previous_element == current
 
     if previous_incrementing.nil?
       previous_incrementing = current > previous_element
-      unless passes_difference?(a: current, b: previous_element)
-        safe = false
-        break
-      end
+      safe = false && break unless passes_difference?(a: current, b: previous_element)
       next
     end
 
     # Check if still matching incrementing
     current_incrementing = current > previous_element
 
-    if current_incrementing != previous_incrementing
-      safe = false
-      break
-    end
-
-    unless passes_difference?(a: current, b: previous_element)
-      safe = false
-      break
-    end
+    safe = false && break if current_incrementing != previous_incrementing
+    safe = false && break unless passes_difference?(a: current, b: previous_element)
   end
 
   safe
@@ -64,25 +51,18 @@ data.each do |line|
   elements = line.split(' ')
   safe = is_report_safe?(elements:)
 
-  if safe
-    safe_reports << elements
-  else
-    length = elements.count
-    safe_permutation = nil
-    (0..length).each do |i|
-      new_permutation = elements.map(&:clone)
-      new_permutation.delete_at(i)
-      if is_report_safe?(elements: new_permutation)
-        safe_permutation = new_permutation
-        break
-      end
-    end
-    if safe_permutation
-      safe_reports << safe_permutation
-      next
-    end
-    unsafe_reports << elements
+  safe_reports << elements && next if safe
+
+  length = elements.count
+  safe_permutation = nil
+  (0..length).each do |i|
+    new_permutation = elements.map(&:clone)
+    new_permutation.delete_at(i)
+    (safe_permutation = new_permutation) && break if is_report_safe?(elements: new_permutation)
   end
+  
+  safe_reports << safe_permutation && next if safe_permutation
+  unsafe_reports << elements
 end
 
 puts "Total safe: #{safe_reports.count}"
