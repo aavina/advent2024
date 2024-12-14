aaeac36561cb8fd21e9178fb2719210dc7ed3beb
Anthony Avina <anthony@aavina.com>
2024-12-14 15:58:04 -0600

    note

--- 4/part1.rb
+++ 4/part1.rb
@@ -13,3 +13,15 @@ data.each do |line|
 end
 
 puts stripped_lines
+
+# TODO: Everytime a `X` is found, need to check:
+# 1. right-to-left
+# 2. left-to-right
+# 3. up-to-down
+# 4. down-to-up
+# 5. diagnoal: down right
+# 6. diagonal: down left
+# 7. diagonal: up left
+# 8. diagonal: up right
+# Note that the "base" is the current position so no need
+# for starting to check in the middle of "XMAS"
